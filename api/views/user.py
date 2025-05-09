bbd022560715cd0a75b099fc3473a5b403bf23f7
Aleksei Alekseev <angelpsymail@gmail.com>
2025-05-09 15:17:06 +0000

    remove uneccesary method in views/user

--- api/views/user.py
+++ api/views/user.py
@@ -7,11 +7,3 @@ from ..serializers.user import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all().order_by('-created_at')
     serializer_class = UserSerializer
-
-    def create(self, request, *args, **kwargs):
-        """Override create to handle custom response."""
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
