84b7b44e4cb0de00bcb64c0eb19b1ae03611e081
Aleksei Alekseev <angelpsymail@gmail.com>
2025-05-13 08:24:19 +0000

    add swagger and redoc docs for api

--- api/models/user.py
+++ api/models/user.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    age = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255, help_text="The full name of the user.")
+    email = models.EmailField(unique=True, help_text="The unique email address of the user.")
+    age = models.PositiveIntegerField(help_text="The age of the user in years.")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="The date and time when the user was created.")
 
     def __str__(self):
         return self.name
