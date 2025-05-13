b0f0cd398b62bee9ce4eccb402ab7198ca7bfbf1
Aleksei Alekseev <angelpsymail@gmail.com>
2025-05-13 19:52:36 +0000

    add debug_toolbar

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls import include
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -44,3 +46,8 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
